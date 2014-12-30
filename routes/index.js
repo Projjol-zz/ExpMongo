7f7ef3b24bc5660a84c3e1ccf2e035ace2875e21
Projjol Banerji <probaner23@gmail.com>
2014-12-30 22:46:49 +0530

    barebones app up

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,41 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET Userlist page. */
+router.get('/userlist', function(req, res) {
+    var db = req.db;
+    var collection = db.get('usercollection');
+    collection.find({},{},function(e,docs){
+        res.render('userlist', {
+            "userlist" : docs
+        });
+    });
+});
+
+/* GET New User page. */
+router.get('/newuser', function(req, res) {
+    res.render('newuser', { title: 'Add New User' });
+});
+
+/*POST*/
+router.post('/adduser', function(req,res){
+	var db = req.db;
+	var userName = req.body.username;
+	var userEmail = req.body.useremail;
+
+	var collection = db.get('usercollection') ;
+	collection.insert({
+		"username": userName,
+		"email": userEmail
+	},function(err,doc){
+		if(err){
+			res.send("something went wrong");
+		}
+		else{
+			res.location('userlist');
+			res.redirect('userlist');
+		}
+
+	});
+});
 module.exports = router;
